e67e3c6b07a471fdd3f2afe6da2ee781e4e422f3
Junior-99 <mtheophilus565@gmail.com>
2024-05-16 07:56:53 +0100

    Task 1

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -11,7 +11,7 @@ function processArray(numbers) {
   }
   
   
-  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const numbers = [1, 2, 3, 4, 5,];
   const newArray = processArray(numbers);
   console.log(newArray); 
   
