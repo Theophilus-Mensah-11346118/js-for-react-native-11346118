29737f7989e929fde0bc40f335b031278efdfa6c
Junior-99 <mtheophilus565@gmail.com>
2024-05-18 05:50:44 +0100

    Task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,17 +1,30 @@
+
 function processArray(numbers) {
-    return numbers.map(number => {
-      if (number % 2 === 0) {
-        
-        return number * number;
-      } else {
-       
-        return number * 3;
-      }
-    });
-  }
-  
-  
-  const numbers = [1, 2, 3, 4, 5,];
-  const newArray = processArray(numbers);
-  console.log(newArray); 
-  
+  return numbers.map(number => {
+    if (number % 2 === 0) {
+      return number * number; 
+    } else {
+      return number * 3; 
+    }
+  });
+}
+
+function formatArrayStrings(strings, numbers) {
+  return strings.map((str, index) => {
+    const num = numbers[index];
+    if (num % 2 === 0) {
+      return str.toUpperCase(); 
+    } else {
+      return str.toLowerCase(); 
+    }
+  });
+}
+
+
+const inputArray = [1, 2, 3, 4, 5];
+const processedNumbers = processArray(inputArray);
+console.log('Processed Numbers:', processedNumbers); 
+
+const stringArray = ['One', 'Two', 'Three', 'Four', 'Five'];
+const formattedStrings = formatArrayStrings(stringArray, processedNumbers);
+console.log('Formatted Strings:', formattedStrings); 
